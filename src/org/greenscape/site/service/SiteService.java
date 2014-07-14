6652a2604e53edf9c7db2f3b744e399aa8b050ab
smsajid <smsajid786@gmail.com>
2014-07-14 23:37:40 +0530

    #1 fix

--- src/org/greenscape/site/service/SiteService.java
+++ src/org/greenscape/site/service/SiteService.java
@@ -1,16 +1,16 @@
 /**
- * 
+ *
  */
 package org.greenscape.site.service;
 
 import java.util.Collection;
 
+import org.greenscape.core.model.Site;
 import org.greenscape.core.service.Service;
-import org.greenscape.site.Site;
 
 /**
  * @author Sheikh Sajid
- * 
+ *
  */
 public interface SiteService extends Service {
 
